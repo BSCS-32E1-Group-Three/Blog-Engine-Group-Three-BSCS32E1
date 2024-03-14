b8adea71b7a12612b14cd0b4b7b0b489788c24b4
Peñaflor_Jose_BSCS32E1 <160807078+Penaflor-Jose-BSCS32E1@users.noreply.github.com>
2024-03-14 22:16:59 +0800

    about us page

--- obj/Debug/net8.0/Blog-Engine.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog-Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog-Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb26e6d8e9c610ef264ecac30cf5730770815c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00f40629d7ba077532b9496feb3467de3c722a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog-Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog-Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
